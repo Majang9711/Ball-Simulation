ae538728fe450e077160043e4303be3ea6a0e9e8
Majang9711 <65146431+Majang9711@users.noreply.github.com>
2021-05-25 02:10:05 +0900

    Add files via upload

---
+++ Assets/c#/main/ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public static ball instance;
+
+    public GameObject target_obj;
+    public bool onEnd = false; //공이 엔드라인에 도달했는가
+
+    void Awake() //시작시 공의 위치를 사작점에 맞춰준다.
+    {
+        if (!instance)
+            instance = this;
+               
+    }
+
+    void Start()
+    {
+        Vector3 pos = target_obj.transform.position;
+        transform.position = new Vector3(pos.x, pos.y + 1, pos.z);
+    }
+
+    public bool getEndLine()
+    {
+        return onEnd;
+    }
+
+   
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "endLine")
+        {
+            onEnd = true;
+        }
+    }
+
+}
---
+++ Assets/c#/main/setting.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class setting : MonoBehaviour
+{
+
+    public static setting instance;
+
+    public float distance = 100; //거리 m기준임(z)
+    public float slope = 10f; //x축 기준 경사각도
+    public float ball_mass = 1f; //공의 질량
+
+    public GameObject ball;
+    public GameObject cube;
+
+    public Rigidbody rb;
+
+    void Awake()
+    {
+        this.Mass();
+
+        this.Slope();
+
+        this.Distance();
+
+        if (!instance)
+            instance = this;
+    }
+
+    public float getDistance()
+    {
+        return distance;
+    }
+
+    public float getBallMass()
+    {
+        return ball_mass;
+    }
+
+    public float getSlope()
+    {
+        return slope;
+    }
+
+    void Mass()//볼 질량
+    {
+        rb = ball.GetComponent<Rigidbody>();
+        rb.mass = ball_mass;
+    }
+
+    void Slope() //경사각도
+    {
+        Quaternion rotate = cube.transform.rotation;
+        cube.transform.rotation = Quaternion.Euler(slope, 0, 0);
+    }
+
+    void Distance() //거리
+    {
+        Vector3 localScalee = cube.transform.localScale;
+        cube.transform.localScale = new Vector3(localScalee.x, localScalee.y + 1, distance);
+    }
+
+}
